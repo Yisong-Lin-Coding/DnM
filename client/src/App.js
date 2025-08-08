3930b93ad979039f65eb91fc0a6d8cdaca1d2025
Plant Lin <mtsacyl4@gmail.com>
2025-08-07 17:41:30 -0700

    PLEASE WORK

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import io from "socket.io-client"
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import StartScreen from './pages/startMenu';
+
+// Correct socket connection string
+const socket = io.connect("http://localhost:3001");
+
+// Component for main menu
+function Home() {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    navigate('/start'); // Use route path, not file path
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome to the D&D Game</h1>
+      <button onClick={handleStart}>Start</button>
+      <button>Settings</button>
+      <button>Exit</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router basename="/DnM">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<StartScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/startMenu.js
@@ -0,0 +1,12 @@
+function startMenu() {
+return(
+    <div>
+        <input placeholder="Username..."/>
+        <input type="password" placeholder="Password..." />
+        <button>Log In</button>
+    </div>
+)
+}
+
+
+export default startMenu
