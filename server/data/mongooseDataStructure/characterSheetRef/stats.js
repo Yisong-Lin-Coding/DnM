346b1314d164221360f04ace2cfbb80731ec06bd
Plant Lin <mtsacyl4@gmail.com>
2025-08-09 13:27:36 -0700

    KYS x3

--- server/data/mongooseDataStructure/characterSheetRef/stats.js
+++ server/data/mongooseDataStructure/characterSheetRef/stats.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const numberRef = require("./numberRef");
 
 const statSchema = new mongoose.Schema({
-    STR: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    DEX: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    CON: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    INT: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    WIS: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    CHA: {type:numberRef, default: { total: 10, permMods: {base:10}, tempMods: {} }},
-    LUCK: {type:numberRef, default: { total: 0, permMods: {base:0}, tempMods: {} }}
+    STR: numberRef,
+    DEX: numberRef,
+    CON: numberRef,
+    INT: numberRef,
+    WIS: numberRef,
+    CHA: numberRef,
+    LUCK: numberRef
 })
+
+module.exports = mongoose.model("Stat", statSchema);
