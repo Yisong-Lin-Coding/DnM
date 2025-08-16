3cb1930792de4f98fc671d183d2f742b420402a7
Plant Lin <mtsacyl4@gmail.com>
2025-08-16 11:02:29 -0700

    Work x10

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const APIHandler = require("./handlers/apiHandler");
 require("dotenv").config();
 
@@ -19,11 +19,27 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(require("./api/login"));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const dataBase = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }})
 
+  async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await dataBase.connect();
+    // Send a ping to confirm a successful connection
+    await dataBase.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await dataBase.close();
+  }
+  }
+
+  run().catch(console.dir);
 
 
 io.on("connection", (socket) => {
