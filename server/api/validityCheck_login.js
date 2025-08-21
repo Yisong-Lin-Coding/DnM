986d909ce500cb7bbb8bf186d6410c0f469ba096
Plant Lin <mtsacyl4@gmail.com>
2025-08-20 20:22:36 -0700

    WORK x31

--- server/api/validityCheck_login.js
+++ server/api/validityCheck_login.js
@@ -1,11 +1,26 @@
-
+const Player = require(`../data/mongooseDataStructure/player`)
 
 module.exports = (socket)=> {
 
     socket.on("validityCheck_login", async (data, callback) => {
         const {playerID, sessionID} = data
 
-        
+        try{
+            const player = await Player.findByID(playerID)
+            if(player._id.toString() == playerID & player.sessionID == sessionID){
+                console.log(`Success\nPlayerID: ${playerID}\nSessionID: ${sessionID}`)
+                callback({sucess:true, message:`This player has been validated`})
+            }
+            else{
+                callback({sucess:false, message: `This player has been invalidated`})
+            }
+
+        }
+        catch{
+            console.error("Validity Error", error) 
+            callback({success:false, error:"An error has occured "})
+        }
+
     }
 
 )}
