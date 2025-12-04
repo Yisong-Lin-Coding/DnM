8bebb16a7279b414c7b659d7881efd523575bf3d
Plant Lin <mtsacyl4@gmail.com>
2025-12-03 17:37:06 -0800

    V.04.05.01

--- server/data/mongooseDataStructure/characterSheetRef/appliedEffects.js
+++ server/data/mongooseDataStructure/characterSheetRef/appliedEffects.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const appliedEffectSchema = new mongoose.Schema({
-    effectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Effect', required: true },
+    effectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Effect', default:"poison-A001" },
     duration: { type: Number, default: 0 }, // Duration in seconds
     appliedAt: { type: Date, default: Date.now },
-    source: { type: mongoose.Schema.Types.ObjectId, ref: 'Character', required: true },
+    source: { type: mongoose.Schema.Types.ObjectId, ref: 'Character', default:"self" },
 })
 
 module.exports = appliedEffectSchema;
